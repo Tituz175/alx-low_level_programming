caa2657a628aca3de71dd81c793dba795760b28c
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-05 07:51:04 -0700

    task 0 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("_putchar\n")
+return (0);
+}
