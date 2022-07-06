c411e136e458474a92707e7b7fe9eeed853131eb
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-05 20:50:36 -0700

    tashk 1 finished

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
-/*
- * print_alphabet - prints the lowercase alphabets
- */
+/**
+ * print_alphabet -> prints the lowercase alphabets
+ *
+ * Return: void
+*/
 
 void print_alphabet(void)
 {
-    int i;
-    
-    for(i = 'a'; i <= 'z'; i++)
-    {
-            _putchar(i);
-    }
-    _putchar('\n');
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
