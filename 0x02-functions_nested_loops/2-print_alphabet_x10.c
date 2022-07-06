63827712de06dc1a838abf486142d1ff704de650
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-05 21:10:43 -0700

    task 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> print the lowercase alphabet ten times on a
+ * different line
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
