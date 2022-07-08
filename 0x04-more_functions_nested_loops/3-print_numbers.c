b6ecd74f283d16eb5e2c6888af7d0bc58c1436e7
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-08 04:34:38 +0100

    more function task 4 done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print number from 0 10 9 on the same line.
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+}
+
