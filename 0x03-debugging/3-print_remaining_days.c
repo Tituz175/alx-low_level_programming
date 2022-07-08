89b1dd2c5c655035515d2913a6886572a25eeeb0
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-08 02:57:21 +0100

    betty wahala

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,11 +16,8 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day >= 60)
 		{
-            printf("Day in leap before: %d\n", day);
 			day++;
-            printf("Day in leap before: %d\n", day);
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
