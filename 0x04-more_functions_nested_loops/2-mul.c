1b4657dd52fad92304d72c41413e35bfbfc59d8f
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-08 04:23:39 +0100

    more function task 3 done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiply two int.
+ * @a: first int number
+ * @b: second int number
+ * Return: 1 if c is digit between 0 and 9, 0 otherwise
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
