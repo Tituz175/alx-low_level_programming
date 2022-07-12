22391809769deba428aeb4d4adc5bfc351576e0c
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-12 23:06:54 +0100

    pointer project task 2 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the lenght of a string.
+ *
+ * @s:the string to be counted.
+ *
+ * Return: int.
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	while (*(s + len) != 0)
+	{
+		len++;
+	}
+
+	return (len);
+}
+
