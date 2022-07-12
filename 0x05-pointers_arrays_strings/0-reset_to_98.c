3d93590a8b62042fee2f1b7a658a51e1b537571a
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-12 21:52:11 +0100

    pointers project task 0 done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the pointer value to 98
+ * @n:my variable in the reset_to_98 function
+ *
+ * return => 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
