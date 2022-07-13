845a927599b51fedd8bc5be8450f91670b974b21
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-13 01:23:30 +0100

    pointer project task 5 done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
+
 #include "main.h"
 
 /**
- * print_rev- prints a string using _putchar in reverse form.
- *
- * @s: this is the string to be printed.
- *
- */
-
+* rev_string -> reversing a string
+* @s: parameter s
+*/
 void rev_string(char *s)
 {
-	int len = 0;
+	char tmp;
+	int i, length1, length2;
 
-	while (s[len] != 0)
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
 	{
-		len++;
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
 	}
-	len--;
-	while (len >= 0)
-	{
-		_putchar(s[len]);
-		len--;
-	}
-	_putchar('\n');
 }
-
