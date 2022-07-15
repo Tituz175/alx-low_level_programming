741f52c217f9eba927e2a2053a8c415251bbac5b
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-15 01:03:31 +0100

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * @brief 
- * 
- * @param dest 
- * @param src 
- * @return char* 
- */
-
-
+* _strcat -> this is a funcion strcat
+* @dest: first param
+* @src: second param
+* Return: a string
+*/
 char *_strcat(char *dest, char *src)
 {
-        int len = 0 ,i;
-        while (dest[len])
-        {
-                len++;
-        }
-        for ( i = 0; src[i] != 0; i++)
-        {
-                dest[len] = src[i];
-                len++;
-        }
-        dest[len] = 0;
-        return (dest);
+	int len = 0, i;
+
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
 }
