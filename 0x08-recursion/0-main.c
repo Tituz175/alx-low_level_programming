4e0eee595e7eebc095403df5d0c07686110fd999
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-21 03:27:04 +0100

    recursion project 2 done

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -1,5 +1,11 @@
 void _puts_recursion(char *);
 void _print_rev_recursion(char *s);
+
+
+#include "main.h"
+#include <stdio.h>
+
+int _strlen_recursion(char *s);
 /**
  * main - check the code for Holberton School students.
  *
@@ -8,6 +14,8 @@ void _print_rev_recursion(char *s);
 int main(void)
 {
 	/*_puts_recursion("First, solve the problem. Then, write the code");*/
-        _print_rev_recursion("First, solve the problem. Then, write the code");
+        /*_print_rev_recursion("First, solve the problem. Then, write the code");*/
+        int result = _strlen_recursion("code");
+        printf("%d\n", result);
 	return (0);
 }
