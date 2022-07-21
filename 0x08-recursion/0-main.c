d77ac3e2e2f814b10ce259035281e7ce50cdd7e3
Tobi Oyekanmi <tobititus.oyekanmi@gmail.com>
2022-07-21 03:00:54 +0100

    recursion project 1 done

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -1,5 +1,5 @@
 void _puts_recursion(char *);
-
+void _print_rev_recursion(char *s);
 /**
  * main - check the code for Holberton School students.
  *
@@ -7,6 +7,7 @@ void _puts_recursion(char *);
  */
 int main(void)
 {
-	_puts_recursion("First, solve the problem. Then, write the code");
+	/*_puts_recursion("First, solve the problem. Then, write the code");*/
+        _print_rev_recursion("First, solve the problem. Then, write the code");
 	return (0);
 }
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - this function print a string from behind recursively.
+ *
+ * @s: is a character
+ *
+ * Return: On success 1.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
+
