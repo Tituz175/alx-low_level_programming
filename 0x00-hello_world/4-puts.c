fb5eb5a867d28bcf37d97ce58acdbab413086c82
Tituz175 <tobititus01@gmail.com>
2023-02-16 21:35:06 -0700

    task 4 fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - print exactly "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
