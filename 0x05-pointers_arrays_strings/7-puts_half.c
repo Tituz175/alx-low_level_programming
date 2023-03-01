4758ba7c33f39a8e2bce0a1c644926997772eee7
Tituz175 <tobititus01@gmail.com>
2023-02-28 21:10:30 -0700

    task 7 fixed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,10 +7,16 @@
 */
 void puts_half(char *str)
 {
-	int len, i;
+	int len, start, i;
 
 	len = strlen(str);
-	for (i = len/2; i < len; i++)
+
+	if (len % 2 == 1)
+		start = len / 2 + 1;
+	else
+		start = len / 2;
+
+	for (i = start; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
