18a655e49bfef41ff0e2fc9bc949032056e80add
Tituz175 <tobititus01@gmail.com>
2023-03-14 21:49:23 -0600

    task 1, malloc done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strdup -> this function makes a copy of a given
+ * string to another allocated memory.
+ * @str: string to be dupliated
+ * Return: a pointer to a newly allocated space.
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int len = 0;
+	int count = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + count) != '\0')
+	{
+		len++;
+		count++;
+	}
+
+	s = (char *)malloc(len * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (count = 0; count < len; count++)
+	{
+		*(s + count) = *(str + count);
+	}
+
+	return (s);
+}
