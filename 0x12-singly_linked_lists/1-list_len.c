79ee5fd6cbd7283b8300acc038fdeb1476cdd78b
Tituz175 <tobititus01@gmail.com>
2023-03-30 19:57:31 -0600

    task 1, singly link list done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,21 @@
 #include "lists.h"
 
+/**
+ * list_len -> this function prints the length of a
+ * singly link list.
+ * @h: the address of the first node.
+ * Return: the length of the list.
+ */
+
 size_t list_len(const list_t *h)
 {
-    size_t output = 0;
+	size_t output = 0;
 
-    while (h != NULL)
-    {
-        output++;
-        h = h->next;
-    }
-    
-    return (output);
+	while (h != NULL)
+	{
+		output++;
+		h = h->next;
+	}
+
+	return (output);
 }
