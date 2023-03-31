ca2316e418ef9f61dfed5365a7df71fa83851cf1
Tituz175 <tobititus01@gmail.com>
2023-03-30 20:09:38 -0600

    task 2, singly link list done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,20 +1,27 @@
 #include "lists.h"
 
+/**
+ * add_node -> this function add a new node at
+ * the start of a singly link list.
+ * @head: the address to the first node.
+ * @str: the string to be copied to the node.
+ * Return: the address of the new node.
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *newNode;
-    int len = strlen(str);
+	list_t *newNode;
+	int len = strlen(str);
 
-    newNode = malloc(sizeof(list_t));
+	newNode = malloc(sizeof(list_t));
 
-    if (newNode == NULL)
-        return (NULL);
+	if (newNode == NULL)
+		return (NULL);
 
-    newNode->str = strdup(str);
-    newNode->len = len;
-    newNode->next = *head;
-    *head = newNode;
+	newNode->str = strdup(str);
+	newNode->len = len;
+	newNode->next = *head;
+	*head = newNode;
 
-
-    return (*head);
+	return (*head);
 }
