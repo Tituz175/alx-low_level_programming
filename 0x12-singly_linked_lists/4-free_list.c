257618db1d9a4410c8461d200348ae4cab365c1b
Tituz175 <tobititus01@gmail.com>
2023-03-30 20:27:11 -0600

    task 4, singly link list done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,20 @@
 #include "lists.h"
 
+/**
+ * free_list -> this function free the memory of
+ * a singly link list.
+ * @head: the address of the first node of the list.
+ * Return: void.
+ */
+
 void free_list(list_t *head)
 {
-    list_t *currentNode;
+	list_t *currentNode;
 
-    while ((currentNode = head) != NULL)
-    {
-        head = head->next;
-        free(currentNode->str);
-        free(currentNode);
-    }
+	while ((currentNode = head) != NULL)
+	{
+		head = head->next;
+		free(currentNode->str);
+		free(currentNode);
+	}
 }
