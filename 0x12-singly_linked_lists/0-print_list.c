67941d828db949e43f35696db563dda237ffd1be
Tituz175 <tobititus01@gmail.com>
2023-03-30 20:11:52 -0600

    task 0, singly link list fixed again

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,21 +8,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t output;
+	size_t nelem;
 
-	output = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
-		if (h->str != NULL)
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		else
-		{
+		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
-		}
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		output++;
+		nelem++;
 	}
-	return (output);
+	return (nelem);
 }
