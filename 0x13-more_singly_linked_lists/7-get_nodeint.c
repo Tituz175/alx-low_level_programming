c66ea0d50a4821e1aaf4eef92eb20eb1ea5f3430
Tituz175 <tobititus01@gmail.com>
2023-04-04 22:07:00 -0600

    task 5 and 7 fixed

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,13 +12,16 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *newNode;
 
-	unsigned int count;
+	size_t count;
 
 	for (count = 0; count <= index && head != NULL; count++)
 	{
 		newNode = head;
 		head = head->next;
 	}
+
+	if (count < index)
+		return (NULL);
 
 	return (newNode);
 }
