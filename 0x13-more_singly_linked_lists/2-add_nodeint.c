a7af9e5c6b4362a5c4520a6e8cd69e86c33539de
Tituz175 <tobititus01@gmail.com>
2023-04-04 18:22:10 -0600

    task 2 of project 13 without betty done

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *newNode;
+    newNode = malloc(sizeof(listint_t));
+
+    if (newNode == NULL)
+        return NULL;
+    
+
+    newNode->n = n;
+    newNode->next = *head;
+
+    *head = newNode;
+
+    return newNode;
+}
