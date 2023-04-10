1254bff08e1238fd46c929c9f667ac7f05e13b15
Tituz175 <tobititus01@gmail.com>
2023-04-09 21:40:58 -0600

    all task done, with task 1 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,13 +26,13 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	text_len = strlen(text_content);
-
 	if (fd == -1)
 		return (-1);
 
-	if (text_len == 0)
+	if (text_content == NULL)
 		text_content = "";
+
+	text_len = strlen(text_content);
 
 	total_size = write(fd, text_content, text_len);
 
