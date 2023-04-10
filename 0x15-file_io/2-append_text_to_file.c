eaa3fe920597abca9e17fc30a96723109f15cd96
Tituz175 <tobititus01@gmail.com>
2023-04-09 21:09:25 -0600

    all task done, with task 2 betty applied

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,28 +6,35 @@
 #include <fcntl.h>
 #include <string.h>
 
+/**
+ * append_text_to_file -> this function append text at the end of
+ * a file.
+ * @filename: the name of the file
+ * @text_content: the text needed to be append
+ * Return: 1 always if successful else -1
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, content_len, total_size;
+	int fd, content_len, total_size;
 
-    content_len = strlen(text_content);
+	content_len = strlen(text_content);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    if(fd == -1)
-    {
-        close(fd);
-        return (-1);
-    }
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 
-    if(content_len)
-    {
-        total_size = write(fd, text_content, content_len);
-    }
-    close(fd);
+	if (content_len)
+		total_size = write(fd, text_content, content_len);
 
-    if (total_size == -1)
-        return (-1);
-    
-    return (1);
+	close(fd);
+
+	if (total_size == -1)
+		return (-1);
+
+	return (1);
 }
