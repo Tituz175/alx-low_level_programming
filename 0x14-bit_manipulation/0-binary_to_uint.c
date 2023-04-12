0c988a52be146cfd2cf6069bb6851b06567c02e1
Tituz175 <tobititus01@gmail.com>
2023-04-11 19:55:59 -0600

    task 0 of bit manipulation done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* binary_to_uint -> this function changes a given number
+* in binary to unsigned int.
+* @b: this is the given number.
+* Return: the unsigned int.
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result;
+	int count;
+
+	result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (count = 0; b[count] != '\0'; count++)
+	{
+		if (b[count] != '0' && b[count] != '1')
+			return (0);
+	}
+
+	for (count = 0; b[count] != '\0'; count++)
+	{
+		result <<= 1;
+
+		if (b[count] == '1')
+			result += 1;
+	}
+
+	return (result);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+# ALX Software Enginering 0x14. C - Bit manipulation
