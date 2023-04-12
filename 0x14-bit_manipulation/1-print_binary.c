cb7d028abc58144ccecfbfca6be517a50457073d
Tituz175 <tobititus01@gmail.com>
2023-04-11 20:50:56 -0600

    task 1 of bit manipulation done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+* print_binary -> this function prints the binary representation
+*  of the given number.
+* @n: this is the given number.
+*/
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, checker;
+	char stopper, ch;
+
+	stopper = 0;
+	divisor = pow(2, sizeof(unsigned long int) * 8 -1);
+
+	while (divisor != 0)
+	{
+		checker = n & divisor;
+
+		if (checker == divisor)
+		{
+			stopper = 1;
+			ch = '1';
+		}
+		else if(stopper == 1 || divisor == 1)
+			ch = '0';
+
+		_putchar(ch);
+
+		divisor >>= 1;
+	}
+}
