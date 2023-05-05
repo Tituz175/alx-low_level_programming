f185a1acb7319b6d1ad13a392a2ec419d38a27b1
Tituz175 <tobititus01@gmail.com>
2023-05-04 18:49:45 -0600

    0x14 major tasks done

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the value of a bit at a given index in a number.
+ * @n: given number
+ * @index: position of the bit
+ * Return: value of the bit else -1 if there is an error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int divisor, checker;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
+	divisor = 1 << index;
+	checker = n & divisor;
+
+	if (checker == divisor)
+		return (1);
+	return (0);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+unsigned long int _power(unsigned int base_num, unsigned int power_num);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+
+#endif
