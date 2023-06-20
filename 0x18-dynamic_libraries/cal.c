2d4edf0b1ca4c645f0582f0f460b33d81c50894b
Tituz175 <tobititus01@gmail.com>
2023-06-19 18:25:50 -0600

    task 2 done

---
+++ 0x18-dynamic_libraries/cal.c
@@ -0,0 +1,65 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+ * add -> sum two numbers
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub -> subtract two numbers
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul -> multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div -> divide two numbers
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod -> modulus of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
