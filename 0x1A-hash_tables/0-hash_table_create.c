9a7c32fe55538b56bc083f4eb1c7fd42af6c4380
Tituz175 <tobititus01@gmail.com>
2023-07-08 20:51:11 -0600

    task 0 modified inline with the requirement

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,9 +11,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
+	if (table == NULL)
+		return (NULL);
+
 	table->size = size;
 
 	table->array = malloc(sizeof(hash_node_t *) * table->size);
+
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
 	return (table);
 }
