255415029472dccf57edc84b90cfe3e35fe6a6a5
Tituz175 <tobititus01@gmail.com>
2023-07-08 23:26:08 -0600

    task 3 modified again

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,6 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
+		free(dup_value);
 		return (0);
 	}
 
