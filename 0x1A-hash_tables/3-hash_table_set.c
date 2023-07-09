14f2b0856b4c2c64867301505956f60c50adbff1
Tituz175 <tobititus01@gmail.com>
2023-07-08 23:20:41 -0600

    task 3 modified again

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, i;
 	hash_node_t *new_node;
 
-	if (*key == '\0' || *value == '\0')
+	if (ht == NULL || *key == '\0' || *value == '\0')
 	{
 		return (0);
 	}
@@ -33,6 +33,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
