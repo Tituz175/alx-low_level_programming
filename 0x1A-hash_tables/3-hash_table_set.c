c1b1a588ba68d4a73f903af6c6a6635cad20ca37
Tituz175 <tobititus01@gmail.com>
2023-07-08 22:25:51 -0600

    task 3 modified

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -37,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[index]->key, key) == 0)
 		{
 			ht->array[index] = new_node;
-			return (1);
 		}
+		return (1);
 	}
 }
