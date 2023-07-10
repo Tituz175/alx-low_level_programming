17395b5b59b771b5c691146a7be45aed6191abdf
Tituz175 <tobititus01@gmail.com>
2023-07-09 22:19:27 -0600

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,20 +2,31 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int index = 0;
-    hash_node_t *node;
+	unsigned long int index = 0;
+	hash_node_t *node;
+	int comma = 0;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    printf("{");
-    for (; index < ht->size; index++)
-    {
-        if (ht->array[index] != NULL)
-        {
-            node = ht->array[index];
-            printf("'%s': '%s', ", node->key, node->value);
-        }
-    }
-    printf("}\n");
+	printf("{");
+	for (; index < ht->size; index++)
+	{
+		if (ht->array[index] != NULL)
+		{
+			if (comma == 1)
+				printf(", ");
+
+			node = ht->array[index];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);   
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma = 1;
+		}
+	}
+	printf("}\n");
 }
