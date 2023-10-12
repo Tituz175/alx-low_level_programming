5c641f9cd2c4f451ad5d9db62849a4aa9f878b2e
tituz175 <tobititus01@gmail.com>
2023-10-11 22:55:10 -0600

    task 9 started

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * interpolation_search -> this function searches for a value in an array
+ * of integers using the Interpolation search algorithm
+ * @array: this is the given array
+ * @size: this is the size of the given array
+ * @value: this is the search value
+ * Return: the index of the value in the array if found else -1
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+}
